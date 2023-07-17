fc60bbf9ae9b76a067411168a70860cb69afe685
Daeho <eogh234@naver.com>
2023-07-18 04:11:36 +0900

    Add sending Fweet function using FireStore(DB)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,33 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [fweet, setFweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("fweets").add({
+            fweet: fweet,
+            createdAt: Date.now(),
+        });
+        setFweet("");
+    };
+    const onChange = (event) => {
+        const { target: { value } } = event;
+        setFweet(value);
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={fweet}
+                    onChange={onChange}
+                    type="text" 
+                    placeholder="What's on your mind" 
+                    maxLength={120} 
+                />
+                <input type="submit" value={"fweet"} />
+            </form>
+        </div>);
+}
 
 export default Home;
