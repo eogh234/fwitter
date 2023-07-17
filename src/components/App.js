34fd29803d19c45308f7cc72bf113c5feb61699d
Daeho <eogh234@naver.com>
2023-07-18 03:14:25 +0900

    Impl persistence and Add text button for toggle Sign In / Sign Up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import fbase, { authService } from 'fBase';
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing.."}
       <footer>&copy; {new Date().getFullYear()} Fwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if (name === "email") {
@@ -26,17 +27,18 @@ const Auth = () => {
             }
             console.log(data);
         } catch (error) {
-            console.error(error);
+            setError(error.message);
         }
-
     };
+    const toggleAccount = () => setNewAccount((prev) => !prev)
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
